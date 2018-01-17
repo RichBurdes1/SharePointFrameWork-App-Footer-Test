377cd0b4ee02061efeac30476d49349caee92beb
Richard Burdes [DATACOM] <RichardBu@datacom.co.nz>
2018-01-18 09:43:33 +1300

    adding local files up to Git

---
+++ src/extensions/appFooter2React/loc/en-us.js
@@ -0,0 +1,5 @@
+define([], function() {
+  return {
+    "Title": "AppFooter2ReactApplicationCustomizer"
+  }
+});
---
+++ src/extensions/appFooter2React/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAppFooter2ReactApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AppFooter2ReactApplicationCustomizerStrings' {
+  const strings: IAppFooter2ReactApplicationCustomizerStrings;
+  export = strings;
+}
